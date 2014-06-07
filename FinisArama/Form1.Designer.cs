d45adcfda44d83c3cd19c08b32868849105d5aa4
cetinyasar <yasar71@gmail.com>
2014-06-07 22:29:07 +0300

    evvvvv

--- FinisArama/Form1.Designer.cs
+++ FinisArama/Form1.Designer.cs
@@ -38,6 +38,7 @@
 			this.printDocument1 = new System.Drawing.Printing.PrintDocument();
 			this.button2 = new System.Windows.Forms.Button();
 			this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+			this.button3 = new System.Windows.Forms.Button();
 			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
 			this.SuspendLayout();
 			// 
@@ -45,19 +46,20 @@
 			// 
 			this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.textBox1.Location = new System.Drawing.Point(93, 12);
+			this.textBox1.Location = new System.Drawing.Point(108, 15);
+			this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
 			this.textBox1.Multiline = true;
 			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(391, 71);
+			this.textBox1.Size = new System.Drawing.Size(455, 86);
 			this.textBox1.TabIndex = 0;
 			this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
 			// 
 			// label1
 			// 
 			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(13, 19);
+			this.label1.Location = new System.Drawing.Point(15, 23);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(63, 13);
+			this.label1.Size = new System.Drawing.Size(74, 16);
 			this.label1.TabIndex = 1;
 			this.label1.Text = "Aranan Kod";
 			// 
@@ -69,11 +71,12 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			this.dataGridView1.Location = new System.Drawing.Point(16, 89);
+			this.dataGridView1.Location = new System.Drawing.Point(19, 110);
+			this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
 			this.dataGridView1.Name = "dataGridView1";
 			this.dataGridView1.ReadOnly = true;
 			this.dataGridView1.RowHeadersVisible = false;
-			this.dataGridView1.Size = new System.Drawing.Size(555, 479);
+			this.dataGridView1.Size = new System.Drawing.Size(647, 590);
 			this.dataGridView1.TabIndex = 3;
 			// 
 			// listBox1
@@ -81,17 +84,20 @@
 			this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.listBox1.FormattingEnabled = true;
-			this.listBox1.Location = new System.Drawing.Point(577, 38);
+			this.listBox1.ItemHeight = 16;
+			this.listBox1.Location = new System.Drawing.Point(673, 47);
+			this.listBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
 			this.listBox1.Name = "listBox1";
-			this.listBox1.Size = new System.Drawing.Size(301, 238);
+			this.listBox1.Size = new System.Drawing.Size(350, 292);
 			this.listBox1.TabIndex = 4;
 			// 
 			// button1
 			// 
 			this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-			this.button1.Location = new System.Drawing.Point(490, 10);
+			this.button1.Location = new System.Drawing.Point(572, 12);
+			this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
 			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(81, 23);
+			this.button1.Size = new System.Drawing.Size(94, 28);
 			this.button1.TabIndex = 2;
 			this.button1.Text = "Ara";
 			this.button1.UseVisualStyleBackColor = true;
@@ -101,9 +107,9 @@
 			// 
 			this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
 			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(575, 19);
+			this.label2.Location = new System.Drawing.Point(671, 23);
 			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(96, 13);
+			this.label2.Size = new System.Drawing.Size(112, 16);
 			this.label2.TabIndex = 5;
 			this.label2.Text = "Yüklenen Dosyalar";
 			// 
@@ -113,9 +119,10 @@
 			// 
 			// button2
 			// 
-			this.button2.Location = new System.Drawing.Point(794, 518);
+			this.button2.Location = new System.Drawing.Point(926, 638);
+			this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
 			this.button2.Name = "button2";
-			this.button2.Size = new System.Drawing.Size(75, 50);
+			this.button2.Size = new System.Drawing.Size(87, 62);
 			this.button2.TabIndex = 6;
 			this.button2.Text = "Yazdır";
 			this.button2.UseVisualStyleBackColor = true;
@@ -131,11 +138,22 @@
 			this.printPreviewDialog1.Name = "printPreviewDialog1";
 			this.printPreviewDialog1.Visible = false;
 			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(673, 638);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(137, 23);
+			this.button3.TabIndex = 7;
+			this.button3.Text = "Birden fazla olanı bul";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
 			// Form1
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(881, 581);
+			this.ClientSize = new System.Drawing.Size(1028, 715);
+			this.Controls.Add(this.button3);
 			this.Controls.Add(this.button2);
 			this.Controls.Add(this.label2);
 			this.Controls.Add(this.button1);
@@ -143,6 +161,8 @@
 			this.Controls.Add(this.dataGridView1);
 			this.Controls.Add(this.label1);
 			this.Controls.Add(this.textBox1);
+			this.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
 			this.Name = "Form1";
 			this.Text = "Form1";
 			this.Load += new System.EventHandler(this.Form1_Load);
@@ -164,6 +184,7 @@
 		private System.Drawing.Printing.PrintDocument printDocument1;
 		private System.Windows.Forms.Button button2;
 		private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+		private System.Windows.Forms.Button button3;
 	}
 }
 
--- FinisArama/Form1.cs
+++ FinisArama/Form1.cs
@@ -29,10 +29,26 @@ namespace FinisArama
 				string dosyaAdi = Path.GetFileName(s);
 				int adet = 0;
 				StreamReader sr = new StreamReader(s);
+				int tip = 0;
 				while (sr.Peek() >= 0)
 				{
-					string[] tokens = sr.ReadLine().Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
-					if (tokens.Length == 2)
+					string satir = sr.ReadLine().Replace("\"","");
+					
+					//if (satir.Contains("Fins") && satir.Contains("MLI") && satir.Contains("Offer"))
+					if (satir.Contains("Fins;MLI;Offer"))
+					{
+						tip = 1;
+						continue;
+					} 
+					if (satir.Contains("Part Number	Unit Sale (Pieces per package)	 Price  "))
+					{
+						tip = 2;
+						continue;
+					}
+
+
+					string[] tokens = satir.Split(new char[] { ';', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+					if (tokens.Length == 2 && tip == 0)
 					{
 						Ayrinti yeni = new Ayrinti {DosyaAdi = dosyaAdi, Kod = tokens[0], Fiyat = tokens[1]};
 						if (!kayitlar.ContainsKey(yeni.Kod))
@@ -40,6 +56,24 @@ namespace FinisArama
 						else
 							kayitlar[yeni.Kod].Add(yeni);
 						adet ++;
+					}
+					else if (tokens.Length == 3 && tip == 1)
+					{
+						Ayrinti yeni = new Ayrinti { DosyaAdi = dosyaAdi, Kod = tokens[0], Fiyat = tokens[2] };
+						if (!kayitlar.ContainsKey(yeni.Kod))
+							kayitlar.Add(yeni.Kod, new List<Ayrinti> { yeni });
+						else
+							kayitlar[yeni.Kod].Add(yeni);
+						adet++;
+					}
+					else if (tokens.Length == 3 && tip == 2)
+					{
+						Ayrinti yeni = new Ayrinti { DosyaAdi = dosyaAdi, Kod = tokens[0], Fiyat = tokens[2] };
+						if (!kayitlar.ContainsKey(yeni.Kod))
+							kayitlar.Add(yeni.Kod, new List<Ayrinti> { yeni });
+						else
+							kayitlar[yeni.Kod].Add(yeni);
+						adet++;
 					}
 				}
 				sr.Close();
@@ -93,6 +127,37 @@ namespace FinisArama
 			Bitmap bm = new Bitmap(this.dataGridView1.Width, this.dataGridView1.Height);
 			dataGridView1.DrawToBitmap(bm, new Rectangle(0, 0, this.dataGridView1.Width, this.dataGridView1.Height));
 			e.Graphics.DrawImage(bm, 0, 0);
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			BindingList<Ayrinti> satirlar = new BindingList<Ayrinti>();
+			
+			foreach (KeyValuePair<string, List<Ayrinti>> s in kayitlar)
+			{
+				if (s.Value.Count > 1)
+				{
+					foreach (var ayrinti in kayitlar[s.Key])
+					{
+						satirlar.Add(ayrinti);
+					}
+				}
+				
+			}
+
+			dataGridView1.DataSource = null;
+			dataGridView1.DataSource = satirlar;
+
+			dataGridView1.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+			dataGridView1.Columns[2].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+			dataGridView1.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+
+			dataGridView1.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.None;
+			dataGridView1.Columns[2].AutoSizeMode = DataGridViewAutoSizeColumnMode.None;
+			dataGridView1.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.None;
+
+			dataGridView1.Refresh();
+
 		}
 	}
 
